6ba0f462ecd332b926c1b3f34cee90efe28f1a61
WoodforkDevinSDI <dmwoodfork@fullsail.edu>
2016-05-03 19:39:08 -0500

    Class Complete
    All files from class

---
+++ Project4 3/js/xcredit.js
@@ -0,0 +1,15 @@
+<SCRIPT LANGUAGE="JavaScript">
+
+<!-- Original:  Francis Woodhouse (francis@contessa.u-net.com) -->
+
+
+
+<!-- Begin
+function runClock() {
+theTime = window.setTimeout("runClock()", 1000);
+var today = new Date();
+var display= today.toLocaleString();
+status=display;
+}
+// End -->
+</SCRIPT>
